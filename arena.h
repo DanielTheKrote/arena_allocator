25f7dcbf2c3daea01e2e56b1681ab6a9c7b94c37
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2026-04-01 10:44:52 -0300

    feat: Add new arena struct and a function to create one

--- arena.h
+++ arena.h
@@ -1,4 +1,31 @@
 #ifndef _ARENA_H
 #define _ARENA_H
 
+#include <stdio.h>
+#include <stdlib.h>   
+
+#define DEFAULT_ARENA_CAPACITY 1024 * 1024 // 1MB
+
+typedef struct
+{
+    size_t capacity;
+    size_t offset;
+    char** data;
+} arena_t;
+
+arena_t*
+create_new_arena()
+{
+    char** data = malloc(sizeof(char) * DEFAULT_ARENA_CAPACITY);
+    if (NULL == data)
+    {
+        return NULL;
+    }
+    arena_t* new_arena = malloc(sizeof(*new_arena));
+    new_arena->capacity = DEFAULT_ARENA_CAPACITY;
+    new_arena->offset = 0;
+    new_arena->data = data;
+    return new_arena;
+}
+
 #endif
--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include "arena.h"
 
-
 int
+
 main(int argc, char* argv[])
 {
-    printf("Hello, World");
+    arena_t* arena = create_new_arena();
+    if (NULL == arena)
+    {
+        fprintf(stderr, "Error while creating a new arena.");
+        exit(EXIT_FAILURE);
+    }
+    printf("offset: %ld, capacity: %ld, data[addr]: %p\n", arena->offset, arena->capacity, arena->data);
 }
