daf69b23e80c319283a215c65078f69d7819c801
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2026-04-01 14:03:47 -0300

    docs: Add the GPL3 license to arena.h

--- arena.h
+++ arena.h
@@ -1,9 +1,23 @@
+/*
+  Copyright (C) 2026  Daniel Maturano <carlosdanielmaturano@proton.me>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef _ARENA_H
 #define _ARENA_H
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 
 #define DEFAULT_ARENA_CAPACITY 1024 * 1024 // 1MB
 
@@ -20,12 +34,13 @@ struct arena
 arena_t*
 create_new_arena()
 {
-    char* data = malloc(sizeof(char) * DEFAULT_ARENA_CAPACITY);
+    char* data = malloc(sizeof(*data) * DEFAULT_ARENA_CAPACITY);
     if (NULL == data)
     {
         return NULL;
     }
     arena_t* new_arena = malloc(sizeof(*new_arena));
+    
     new_arena->capacity = DEFAULT_ARENA_CAPACITY;
     new_arena->offset = 0;
     new_arena->data = data;
