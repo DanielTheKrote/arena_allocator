bf5e44562c30c55fabd3bde71d8b003afa7a6aa4
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2026-04-01 12:18:04 -0300

    feat: Add a next node to the arena allocator

--- arena.h
+++ arena.h
@@ -1,6 +1,5 @@
 #ifndef _ARENA_H
 #define _ARENA_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,12 +7,15 @@
 
 #define DEFAULT_ARENA_CAPACITY 1024 * 1024 // 1MB
 
-typedef struct
+typedef struct arena arena_t;
+
+struct arena
 {
     size_t capacity;
     size_t offset;
+    arena_t* next; 
     char* data;
-} arena_t;
+};
 
 arena_t*
 create_new_arena()
@@ -27,6 +29,7 @@ create_new_arena()
     new_arena->capacity = DEFAULT_ARENA_CAPACITY;
     new_arena->offset = 0;
     new_arena->data = data;
+    new_arena->next = NULL;
     return new_arena;
 }
 
@@ -36,6 +39,11 @@ free_arena(arena_t* arena)
     arena->capacity = 0;
     arena->offset = 0;
     free(arena->data);
+    if (NULL != arena->next)
+    {
+        free_arena(arena->next);
+        arena->next = NULL;
+    }
 }
 
 void*
@@ -45,8 +53,15 @@ arena_try_alloc(arena_t* arena, size_t size)
     if (size > arena->capacity) // the block does not have enough capacity
         return NULL;
     
-    if (arena->offset + size >= arena->capacity) // not enough memory remaining on the block
-        return NULL;
+    if (arena->offset + size > arena->capacity) // not enough memory remaining on the block
+    {
+        if (NULL == arena->next)
+        {
+            arena->next = create_new_arena();
+            return arena_try_alloc(arena->next, size);
+        }
+        return arena_try_alloc(arena->next, size);
+    }
     
     void* ptr = &arena->data[arena->offset];
     arena->offset += size;
@@ -58,6 +73,9 @@ reset_arena(arena_t* arena)
 {
     memset(arena->data, 0, arena->capacity);
     arena->offset = 0;
+    if (NULL != arena->next)
+        reset_arena(arena->next);
 }
+
 
 #endif
--- main.c
+++ main.c
@@ -16,23 +16,18 @@ main(int argc, char* argv[])
         fprintf(stderr, "Error while creating a new arena.");
         exit(EXIT_FAILURE);
     }
+    
+    for (size_t j = 0; j < 10; j++)
+        (void)arena_try_alloc(arena, 1024 * 512); // 0.5MB
 
-    test_struct_t* t = arena_try_alloc(arena, sizeof(*t));
-    t->x = 157171;
-    t->y = 42069;
-
-
-    char* buff = arena_try_alloc(arena, 1024 * 1024 * 2); // try to allocate 2MB
-    if (NULL == buff)
+    arena_t* curr = arena;
+    while (curr != NULL)
     {
-        printf("the arena does not have memory for the buff.\n");
+        printf("curr[addr]: %p\n" "offset: %ld\n" "1MB: %d\n" "0.5MB: %d\n", curr, curr->offset, 1024 * 1024, 1024 * 512);
+        curr = curr->next;
     }
 
-    printf("t.x = %d, t.y = %d\n", t->x, t->y);
-    reset_arena(arena);
-    printf("t.x = %d, t.y = %d\n", t->x, t->y);
     
-
-    printf("offset: %ld, capacity: %ld, data[addr]: %p\n", arena->offset, arena->capacity, arena->data);
+    reset_arena(arena);
     free_arena(arena);
 }
